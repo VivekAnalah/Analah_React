a9c35fa208737b0643d06a18c9792d23f80d765d
unknown <vivek.belwal@analah.in>
2023-05-31 14:36:51 +0530

    done with font

--- src/Components/Values.js
+++ src/Components/Values.js
@@ -56,7 +56,7 @@ function About() {
         <h2 className="font-semibold text-2xl lg:text-3xl text-center m-auto lg:pb-[30px] pb-[0px]">
         <span className="title-border-bottom">Value</span>s
         </h2>
-        <h5 className="font-normal text-base lg:text-[18px] text-center text-[#595959] my-1 pt-[30px]">
+        <h5 className="font-normal text-base lg:text-[16px] 2xl:text-[18px]  text-center text-[#595959] my-1 pt-[30px]">
           At Analah Insurance, we believe our values shape and define the
           culture of our company. Our values serve the foundation on how we work
           and make our business decisions.
